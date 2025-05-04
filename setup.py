9502cadee73a5fe8d001b6e1231dfadc3cb7924e
yue <yy3098@columbia.edu>
2025-05-03 22:07:44 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 Call `pip install -e .` to install package locally for testing.
 """
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 # build command
 setup(
@@ -15,9 +15,13 @@ setup(
 	license="GPLv3",
 	description="A package for automating photo sorting and 3D model building",
 	classifiers=["Programming Language :: Python :: 3"],
+	packages=find_packages(include=["photogramkit", "photogramkit.*"]), # only install the photogramkit package rather than the notebook package
 	entry_points={
 		"console_scripts": [
 			"photogramkit = photogramkit.__main__:main"
 		]
 	}
+	install_requires=[
+        'pyexiftool==0.4.13'
+    ],
 	)
